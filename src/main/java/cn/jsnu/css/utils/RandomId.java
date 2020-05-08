89984de251bd2d03b0a589571a6c2ecf9b620ea8
BlackCrafts <13160292800@163.com>
2020-05-09 01:06:53 +0800

    0509新增:  -完成user相关接口以及实现;  -修改User和Goods类的部分字段;  -RandomId工具类新增方法;

--- src/main/java/cn/jsnu/css/utils/RandomId.java
+++ src/main/java/cn/jsnu/css/utils/RandomId.java
@@ -13,23 +13,23 @@ public class RandomId {
      * @param length 长度
      * @return 随机字符串
      */
-    public static String getRandomString(int length) {
+    private static String getRandomString(int length) {
         Random random = new Random();
         StringBuffer sb = new StringBuffer();
         for (int i = 0; i < length; i++) {
             int number = random.nextInt(3);
-            long result = 0;
+            long result;
             switch (number) {
                 case 0:
                     result = Math.round(Math.random() * 25 + 65);
-                    sb.append(String.valueOf((char) result));
+                    sb.append((char) result);
                     break;
                 case 1:
                     result = Math.round(Math.random() * 25 + 97);
-                    sb.append(String.valueOf((char) result));
+                    sb.append((char) result);
                     break;
                 case 2:
-                    sb.append(String.valueOf(new Random().nextInt(10)));
+                    sb.append(new Random().nextInt(10));
                     break;
                 default: break;
             }
