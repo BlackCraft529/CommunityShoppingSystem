ff02f4745aec4e0b4e5053448da4493aab052b51
BlackCrafts <13160292800@163.com>
2020-05-11 14:09:15 +0800

    0511-2更新：  -更改部分接口参数列表;

--- src/main/java/cn/jsnu/css/service/GoodService.java
+++ src/main/java/cn/jsnu/css/service/GoodService.java
@@ -11,19 +11,10 @@ import java.util.List;
 public interface GoodService {
     /**
      * 新增一个商品
-     * @param goodsName 商品名
-     * @param goodsSummary 商品概述
-     * @param goodsCate 商品分类好
-     * @param goodsPrice 商品定价
-     * @param goodsSalesPrice 商品销售价
-     * @param goodsRemain 商品库存
-     * @param goodsImage 商品图片链接
-     * @param goodsSales 商品销量
-     * @param goodsCreateTime 创建时间
+     * @param goods 商品
      * @return 影响条数
      */
-    int addGoods(String goodsName, String goodsSummary, String goodsCate,
-                 Double goodsPrice, Double goodsSalesPrice, Integer goodsRemain, String goodsImage, int goodsSales, Date goodsCreateTime);
+    int addGoods(Goods goods);
 
     /**
      * 通过商品ID获取商品
@@ -40,19 +31,9 @@ public interface GoodService {
 
     /**
      * 更新商品信息
-     * @param goodsId 商品ID
-     * @param goodsName 商品名称
-     * @param goodsSummary 商品概述
-     * @param goodsCate 商品分类号
-     * @param goodsPrice 商品定价
-     * @param goodsSalesPrice 商品销售价
-     * @param goodsRemain 商品库存
-     * @param goodsImage 商品图片链接
-     * @param goodsSales 商品销量
-     * @param goodsCreateTime 商品创建时间
+     * @param goods 商品
      */
-    void updateGoods(String goodsId,String goodsName,String goodsSummary,
-                     String goodsCate,double goodsPrice,double goodsSalesPrice,int goodsRemain,String goodsImage,int goodsSales,Date goodsCreateTime);
+    void updateGoods(Goods goods);
 
     /**
      * 更新商品价格
--- src/main/java/cn/jsnu/css/service/Impl/GoodServiceImpl.java
+++ src/main/java/cn/jsnu/css/service/Impl/GoodServiceImpl.java
@@ -20,25 +20,17 @@ public class GoodServiceImpl implements GoodService {
 
     /**
      * 新增一个商品
-     * @param goodsName 商品名
-     * @param goodsSummary 商品概述
-     * @param goodsCate 商品分类好
-     * @param goodsPrice 商品定价
-     * @param goodsSalesPrice 商品销售价
-     * @param goodsRemain 商品库存
-     * @param goodsImage 商品图片链接
-     * @param goodsSales 商品销量
-     * @param goodsCreateTime 创建时间
+     * @param goods 商品
      * @return 影响条数
      */
     @Override
-    public int addGoods(String goodsName, String goodsSummary, String goodsCate,
-                        Double goodsPrice, Double goodsSalesPrice, Integer goodsRemain, String goodsImage, int goodsSales, Date goodsCreateTime) {
+    public int addGoods(Goods goods) {
         String goodsId=RandomId.getRandomGoodId();
         while(findGoodsById(goodsId)!=null){
             goodsId=RandomId.getRandomGoodId();
         }
-        return goodMapper.addGoods(new Goods(goodsId,goodsName,goodsSummary,goodsCate,goodsPrice,goodsSalesPrice,goodsRemain,goodsImage,goodsSales,goodsCreateTime));
+        goods.setGoodsId(goodsId);
+        return goodMapper.addGoods(goods);
     }
 
     /**
@@ -62,21 +54,11 @@ public class GoodServiceImpl implements GoodService {
 
     /**
      * 更新商品信息
-     * @param goodsId 商品ID
-     * @param goodsName 商品名称
-     * @param goodsSummary 商品概述
-     * @param goodsCate 商品分类号
-     * @param goodsPrice 商品定价
-     * @param goodsSalesPrice 商品销售价
-     * @param goodsRemain 商品库存
-     * @param goodsImage 商品图片链接
-     * @param goodsSales 商品销量
-     * @param goodsCreateTime 商品创建时间
+     * @param goods 商品
      */
     @Override
-    public void updateGoods(String goodsId,String goodsName,String goodsSummary,
-                            String goodsCate,double goodsPrice,double goodsSalesPrice,int goodsRemain,String goodsImage,int goodsSales,Date goodsCreateTime) {
-        goodMapper.updateGoods(new Goods(goodsId,goodsName,goodsSummary,goodsCate,goodsPrice,goodsSalesPrice,goodsRemain,goodsImage,goodsSales,goodsCreateTime));
+    public void updateGoods(Goods goods) {
+        goodMapper.updateGoods(goods);
     }
 
     /**
