32afbb1bacdf059fbd42217a0d747afb12d8cb14
BlackCrafts <13160292800@163.com>
2020-05-16 23:12:42 +0800

    0516-1更新:  -ShopCart层接口修复;

---
+++ src/test/java/cn/jsnu/css/service/TestOrderService.java
@@ -0,0 +1,32 @@
+package cn.jsnu.css.service;
+
+import cn.jsnu.css.pojo.Goods;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+/**
+ * @author 魏荣轩-18090248
+ * @date 2020/5/16 22:46
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class TestOrderService {
+
+    @Autowired
+    @Qualifier("ShopCartServiceImpl")
+    private ShopCartService shopCartService;
+
+    @Test
+    public void testGetUserCart(){
+        List<Goods> allGoods=shopCartService.findShopCartByUserId("0da6330bf9b9447d8f305eea29b4ae96");
+        for(Goods goods:allGoods){
+            System.out.println(goods);
+        }
+    }
+}
