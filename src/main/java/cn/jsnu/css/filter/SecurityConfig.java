ea05784d2f61e7c10cbc153f44cb226d66acf671
Double327 <180303665@qq.com>
2020-05-23 13:49:30 +0800

    购物车、订单结算

---
+++ src/main/java/cn/jsnu/css/filter/SecurityConfig.java
@@ -0,0 +1,33 @@
+package cn.jsnu.css.filter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author 李广帅
+ * @date 2020/5/22 7:36 下午
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("13145218799").password("123456");
+    }
+
+}
